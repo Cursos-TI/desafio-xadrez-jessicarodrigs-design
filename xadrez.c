5d1824b1c676bd7739632453e0af240fdd42bd0a
jessicarodrigs-design <jessicarodrigs@gmail.com>
2025-08-31 17:02:28 +0000

    DESAFIO XADREZ

--- xadrez.c
+++ xadrez.c
@@ -2,7 +2,9 @@
 
 int main(){
 
+
 // MOVIMENTO DA TORRE EM FOR
+// NIVEL NOVATO
 
 int casasTorre = 5;
     printf("Movimento da Torre: \n");
@@ -34,6 +36,34 @@ do
     printf("Esquerda\n"); // MOVIMENTO DA RAINHA EM TODAS AS DIREÇÕES
     contadorRainha++;
 }   while (contadorRainha <= casasRainha);
+
+// NIVEL INTERMEDIARIO
+// MOVIMENTO DAS PEÇAS
+    printf("\nMovimento das peças: \n");
+    printf("Cima\n");
+    printf("Direita\n");
+    printf("Baixo\n");
+    printf("Esquerda\n");
+
+    printf("\n");
+
+// MOVIMENTO DO CAVALO: 2 PARA BAIXO E 1 PARA ESQUERDA
+int movimentobaixo = 2;
+int movimentoesquerda = 1;
+    printf("Movimento do Cavalo: \n");
+
+// LOOP EXTERNO: MOVIMENTO PARA BAIXO EM FOR
+for(int i = 0; i < movimentobaixo; i++){
+    printf("Baixo\n");
+
+// LOOP INTERNO: MOVIMENTO PARA ESQUERDA EM WHILE
+int j = 0;
+while(j <movimentoesquerda && i == movimentobaixo - 1){
+    printf("Esquerda\n");
+    j++;
+    }
+   
+}
 
     return 0;
 }
